d135a5aaafec5571a79a966d315bfbc8829cbb49
Matilda Smets <matilda.smets@student.gildeopleidingen.nl>
2024-06-11 13:07:47 +0200

    pekka

---
+++ src/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Issatillie\Pekka;
+
+class Main {
+    public static function run(){
+        echo "  ____      _    _         \n";
+        echo " |  _ \\ ___| | _| | ____ _ \n";
+        echo " | |_) / _ \\ |/ / |/ / _` |\n";
+        echo " |  __/  __/   <|   < (_| |\n";
+        echo " |_|   \\___|_|\\_\\_|\\_\\__,_|\n";
+    }
+}
+
+?>
